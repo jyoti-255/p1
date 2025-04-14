7c22b1df358132d04602af8162b9e62df298bec6
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2025-04-14 17:39:41 +0530

    Update settings.py

--- p1_project/p1_project/settings.py
+++ p1_project/p1_project/settings.py
@@ -123,8 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://p1-6.onrender.com',
-    # Add other trusted origins here
-]
 
