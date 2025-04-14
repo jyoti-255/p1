e8977204850143cbb65db95dff7591299791c58a
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2025-04-14 15:37:30 +0530

    Update settings.py

--- p1_project/p1_project/settings.py
+++ p1_project/p1_project/settings.py
@@ -122,6 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = [
-    'https://p1-4-7pa1.onrender.com'
-]
